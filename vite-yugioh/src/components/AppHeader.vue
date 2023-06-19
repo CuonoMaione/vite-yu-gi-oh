b7b2baab8b7a979110ddcfeb5c140cffdeb8711a
CuonoMaione <cuonomaione99@outlook.it>
2023-06-20 01:06:58 +0200

    add navbar

--- vite-yugioh/src/components/AppHeader.vue
+++ vite-yugioh/src/components/AppHeader.vue
@@ -1,5 +1,13 @@
 <template lang="">
   <header>
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg/1200px-Yu-Gi-Oh%21_%28Logo%29.jpg" alt="Logo" class="d-inline-block align-text-top">
+                Yu-Gi-Oh Api
+            </a>
+        </div>
+    </nav>
   </header>
 </template>
 <script>
@@ -7,6 +15,8 @@ export default {
     name : 'AppHeader',
 }
 </script>
-<style lang="">
-    
+<style lang="scss" scoped>
+    img{
+        width: 9%;
+    }
 </style>
